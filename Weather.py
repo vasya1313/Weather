2c16f4c918ca461ee93b5acb49edce51eb3335b4
Vasya <vasya130393@yandex.ru>
2025-03-27 18:04:29 +0300

    Подключился к openweathermap

---
+++ Weather.py
@@ -0,0 +1,9 @@
+import requests
+
+
+city = 'Оленегорск'
+url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=93f00855b8c77a06ebe3da8aedbccbc4'
+
+weather_data = requests.get(url).json()
+
+print(f'Температура в {city} = {round(weather_data['main']['temp'])}˚C')
