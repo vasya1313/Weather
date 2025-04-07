369cce535904195bbec6ad9b0302803719de3739
Vasya <vasya130393@yandex.ru>
2025-04-07 16:38:21 +0300

    Добавил ввод города

--- Weather.py
+++ Weather.py
@@ -1,9 +1,9 @@
 import requests
 
 
-city = 'оленегорск'
+city = input(str('Введите город '))
 url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=93f00855b8c77a06ebe3da8aedbccbc4'
 
 weather_data = requests.get(url).json()
 
-print(f'Температура в {city} = {round(weather_data['main']['temp'])}˚C')
+print(f'Температура в городе {city}: = {round(weather_data['main']['temp'])}˚C')
