72a81f8e74e005df2fe60d8eef8711ed7a8235a0
Vasya <vasya130393@yandex.ru>
2025-04-07 16:53:12 +0300

    Добавмл обработку исключений при неправильном вводе города

--- Weather.py
+++ Weather.py
@@ -2,8 +2,17 @@ import requests
 
 
 city = input(str('Введите город '))
-url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=93f00855b8c77a06ebe3da8aedbccbc4'
 
-weather_data = requests.get(url).json()
 
-print(f'Температура в городе {city}: = {round(weather_data['main']['temp'])}˚C')
+    
+
+try:
+    url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=93f00855b8c77a06ebe3da8aedbccbc4'
+
+    weather_data = requests.get(url).json()
+    print(f'Температура в городе {city}: = {round(weather_data['main']['temp'])}˚C')
+except:
+    print('Город введен не правильно!')
+    
+    
+    
