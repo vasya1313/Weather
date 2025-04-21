83d834322d51373ee5df33d0fcf8399fabe7cf01
Vasya <vasya130393@yandex.ru>
2025-04-21 17:43:23 +0300

    Добавил poetry

---
+++ weather-prodgect/weather_prodgect/Weather.py
@@ -0,0 +1,18 @@
+import requests
+
+
+while True:
+    city = input(str('Введите город '))
+ 
+
+    try:
+        url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=93f00855b8c77a06ebe3da8aedbccbc4'
+        weather_data = requests.get(url).json()
+        print(f'Температура в городе {city}: = {round(weather_data['main']['temp'])}˚C')
+        break
+
+    except:
+        print('Город введен не правильно!')
+        
+    
+    
